3ddbf3a5eb72a016bdca13ab075eabfe4d5b121c
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2025-03-30 03:46:58 -0400

    quantifier

--- fuzzycontrol/quantifier/one_hot_encoder.py
+++ fuzzycontrol/quantifier/one_hot_encoder.py
@@ -1,4 +1,19 @@
+import numpy as np
+
 from fuzzycontrol.quantifier import BaseQuantifier
+from typing import List
 
 class OneHotEncoder(BaseQuantifier):
-    pass
+    
+    def __init__(self, sentiments:List[str]):
+        self.sentiments = sentiments
+
+    def __call__(self, sentiment:str)->np.array:
+        """
+        One-hot encodes the sentiment.
+        :param sentiment: The sentiment to encode.
+        :return: A list of 0s and 1s representing the one-hot encoding of the sentiment.
+        """
+        one_hot = np.zeros(len(self.sentiments), dtype=int)
+        one_hot[self.sentiments.index(sentiment)] = 1
+        return one_hot
